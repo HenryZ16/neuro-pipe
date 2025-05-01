5adb9cb6a9d46dffc1ae5303220dc505182e0228
HenryZ16 <1546169856@qq.com>
2025-05-01 17:21:24 +0800

    [impl] add config in DPABIProcessor

--- DataProcessor/DPABIProcessor.py
+++ DataProcessor/DPABIProcessor.py
@@ -1,10 +1,69 @@
 from LLMAgent import LLMEnhancedAligner
+import os
 
 
 class DPABIProcessor:
     aligner: LLMEnhancedAligner = None
+    working_dir: str = "./sub-02CB"
+    config: dict = {
+        "DPARSFVersion": None,
+        "WorkingDir": None,
+        "DataProcessDir": None,
+        "SubjectID": None,
+        "TimePoints": None,
+        "TR": None,
+        "IsNeedConvertFunDCM2IMG": None,
+        "IsRemoveFirstTimePoints": None,
+        "RemoveFirstTimePoints": None,
+        "IsSliceTiming": None,
+        "SliceTiming": {
+            "SliceNumber": None,
+            "TR": None,
+            "TA": None,
+            "SliceOrder": None,
+            "ReferenceSlice": None,
+        },
+        "IsRealign": None,
+        "IsNormalize": None,
+        "IsNeedConvertT1DCM2IMG": None,
+        "Normalize": {
+            "BoundingBox": None,
+            "VoxSize": None,
+            "AffineRegularisationInSegmentation": None,
+        },
+        "IsSmooth": None,
+        "Smooth": {"FWHM": None},
+        "IsDetrend": None,
+        "IsCovremove": None,
+        "Covremove": {
+            "PolynomialTrend": None,
+            "HeadMotion": None,
+            "WholeBrain": None,
+            "CSF": None,
+            "WhiteMatter": None,
+            "OtherCovariatesROI": None,
+        },
+        "MaskFile": None,
+        "IsCalALFF": None,
+        "CalALFF": {"AHighPass_LowCutoff": None, "ALowPass_HighCutoff": None},
+        "IsFilter": None,
+        "Filter": {
+            "Timing": "AfterNormalize",
+            "ALowPass_HighCutoff": None,
+            "AHighPass_LowCutoff": None,
+            "AAddMeanBack": None,
+        },
+        "IsCalReHo": None,
+        "CalReHo": {"ClusterNVoxel": None, "smReHo": None},
+        "IsCalFC": None,
+        "IsExtractROISignals": None,
+        "CalFC": {"IsMultipleLabel": None, "ROIDef": None},
+        "StartingDirName": None,
+    }
 
     def preprocess_data(self, input: dict) -> dict:
+        self.config["DPARSFVersion"] = "NOUSE"
+        self.config["WorkingDir"] = os.path.abspath(self.working_dir)
         pass
 
     def generate_report(self) -> str:
